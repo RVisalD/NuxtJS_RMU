620a5b9c134c13f671196724017e11547de83f99
RVisalD <126460312+RVisalD@users.noreply.github.com>
2024-05-30 00:07:41 +0700

    fixing icon in navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -32,7 +32,7 @@ const logout = async () => {
     class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md px-10"
   >
     <NuxtLink class="text-3xl font-mono" to="/">
-      <NuxtImg class="h-12" src="../public/rmu.png" />
+      <NuxtImg class="h-12" src="/rmu.png" />
     </NuxtLink>
     <div
       v-if="user && user.user_metadata && user.user_metadata.name"
